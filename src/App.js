1bb466b98e2faa3bb6cd0e9dd38bce918b2d2c41
babajankhan <babajan.patan@wealthy.in>
2023-11-06 15:27:42 +0530

    added react file structure demo

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
     <Router>
       <div>
         <Navbar />
-        {/* <Hero /> */}
         <Routes>
           <Route path="/"  element={<Hero />} />
           <Route path="/pill-entry" element={<PillEntry />} />
